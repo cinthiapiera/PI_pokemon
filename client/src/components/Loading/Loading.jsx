e0b24cd2b9ae23154f3f3e5fdf075945c9ea6418
cinthiapiera <cinthiapiera>
2022-11-11 11:53:47 -0500

    frontend terminado

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-// import styles from "./Loading.module.css";
+import styles from "./Loading.module.css";
 
 function Loading() {
   return(
     <div>
-      {/* <div className={styles.shadow}></div>
-        <div className={styles.pokeball}>
-            <div className={styles.top}></div>
-            <div className={styles.bottom}></div>
-            <div className={styles.middle}></div>
-      </div> */}
-      <p>Loading component...</p>
+      <div className={styles["shadow"]}></div>
+        <div className={styles["pokeball"]}>
+            <div className={styles["top"]}></div>
+            <div className={styles["bottom"]}></div>
+            <div className={styles["middle"]}></div>
+      </div>
     </div>
   )
 }
--- client/src/components/Page404/Page404.jsx
+++ client/src/components/Page404/Page404.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./Page404.module.css";
+import img from "./../../image/404page.png";
 import { Link } from "react-router-dom";
 
 function Page404() {
     return(
-        <div className={styles.container}>
-            <p>404 | This page could not be found.</p>
-            <Link to="/home"><button>Regresar a Home</button></Link>
-        </div>
+        <div className={styles["page-container"]}>
+            <img src={img} alt="404 not found" width="500px" heigth="300px"/>
+            <div className={styles["page-title"]}>
+                <p>The page you were looking doesn't exist</p>
+            <Link to="/home"><button className={styles["page-button-back"]}> 🡠 Back to home</button></Link>
+            </div>
+        </div>    
     )
 }
 
