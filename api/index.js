752b2e90d0f5737fb3ab9920d99eb3f7987767f3
cinthiapiera <cinthiapiera>
2022-12-01 01:18:09 -0500

    backend terminado

--- api/index.js
+++ api/index.js
@@ -19,12 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { getTypes } = require('./src/controllers/typesC')
+const { getTypes } = require('./src/controllers/typesC');
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {  
-  server.listen(3001, () => {
+  server.listen(port, () => {
     getTypes()
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`Server listening at ${port}`); // eslint-disable-line no-console
   });
 });
