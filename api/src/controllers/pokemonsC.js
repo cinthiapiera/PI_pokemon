5819cd2bddf2029c19f8f4128001ed1f0fe4429b
cinthiapiera <cinthiapiera>
2022-12-02 09:34:45 -0500

    deploy2

--- api/src/controllers/pokemonsC.js
+++ api/src/controllers/pokemonsC.js
@@ -21,11 +21,11 @@ const dataApi = async() => {
       //   }
       // })
       // throw Error('error voluntario')
-    const apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=40')
-    const apiInfo = apiUrl.data.results?.map( async (e) => await axios.get(e.url)) 
-    const pokeInfo = await axios.all(apiInfo)
+    let apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=40')
+    let apiInfo = apiUrl.data.results.map( async (e) => await axios.get(e.url)) 
+    let pokeInfo = await axios.all(apiInfo)
     .then(respuesta => 
-        respuesta?.map( p => { 
+        respuesta.map( p => { 
             return  {
                 id: p.data.id,
                 name: p.data.name,
