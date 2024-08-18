8a23e267151536b92dea370fc13818b3ed4e7129
Cinthiapiera <cinthia.maldonado@upch.pe>
2024-08-18 02:08:36 -0500

    update api

--- api/src/db.js
+++ api/src/db.js
@@ -3,9 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
+  DATABASE_URL
 } = process.env;
 
+//DB_USER, DB_PASSWORD, DB_HOST, 
 // const sequelize = new Sequelize(
 //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
 //   {
@@ -14,7 +15,7 @@ const {
 //   }
 // );
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions: {
